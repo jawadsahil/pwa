54cb4747229537b31851a42f75d0da59f3942ce7
Jawad <jawadsahil@gmail.com>
2022-09-29 19:32:52 +0500

    Create meta.php

---
+++ meta.php
@@ -0,0 +1,39 @@
+<meta name="application-name" content="CMS">
+
+<!-- Android  -->
+<meta name="theme-color" content="#ffffff">
+<meta name="mobile-web-app-capable" content="yes">
+
+<!-- iOS -->
+<meta name="apple-mobile-web-app-title" content="CMS">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="default">
+
+<!-- Windows  -->
+<meta name="msapplication-navbutton-color" content="red">
+<meta name="msapplication-TileColor" content="#ffc40d">
+<meta name="msapplication-TileImage" content="assets/icons/ms-icon-144x144.png">
+<meta name="msapplication-config" content="browserconfig.xml">
+
+<!-- Pinned Sites  -->
+<meta name="msapplication-tooltip" content="Tooltip Text">
+<meta name="msapplication-starturl" content="/">
+
+<!-- Tap highlighting  -->
+<meta name="msapplication-tap-highlight" content="no">
+
+<!-- UC Mobile Browser  -->
+<meta name="full-screen" content="yes">
+<meta name="browsermode" content="application">
+
+<!-- Disable night mode for this page  -->
+<meta name="nightmode" content="enable/disable">
+
+<!-- Layout mode -->
+<meta name="layoutmode" content="fitscreen/standard">
+
+<!-- imagemode - show image even in text only mode  -->
+<meta name="imagemode" content="force">
+
+<!-- Orientation  -->
+<meta name="screen-orientation" content="any">
